879dd81804f1fad101dda00e98e61a53beb318d4
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2022-05-21 15:25:58 +0500

    set up the class that will observe the cpu usage

--- src/cCpuStats.hpp
+++ src/cCpuStats.hpp
@@ -2,26 +2,51 @@
 #include <fstream>
 #include <sstream>
 #include <thread>
+#include <chrono>
+#include <mutex>
 
-
+///////////////////////////////////////////////////////////////////////////////
+/////////////// CLASS DECLARATION /////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * @brief This is a class to read and calculate the cpu usage statistics
+*/
 class cCpuStats
 {
 private:
+  /// For calculating the CPU percentage
   uint32_t lastTotalUser = 0;
   uint32_t lastTotalUserLow = 0;
   uint32_t lastTotalSys = 0;
   uint32_t lastTotalIdle = 0;
 
-  std::thread statThread;
+  double m_currCpuUsage = 0.0;
+
+  /// Thread stuff
+  std::shared_ptr<std::thread> m_statThread;
+  std::mutex m_statMutex;
+
+  void UpdateCpuUsage();
+
+  void LaunchObserver();
 
   
 public:
   cCpuStats(/* args */);
   ~cCpuStats();
 
-  double getCurrVal();
+  void StartReadThread();
+
+  inline double GetCurrCpuUsage()
+  {
+    return m_currCpuUsage;
+  }
 
 };
+
+///////////////////////////////////////////////////////////////////////////////
+////////////////////// METHOD DEFINITIONS /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 cCpuStats::cCpuStats(/* args */)
 {
@@ -30,10 +55,11 @@ cCpuStats::cCpuStats(/* args */)
 
 cCpuStats::~cCpuStats()
 {
+  m_statThread->join();
 }
 
-
-double cCpuStats::getCurrVal()
+///////////////////////////////////////////////////////////////////////////////
+void cCpuStats::UpdateCpuUsage()
 {
   u_int totalUser, totalUserLow, totalSys, totalIdle, total;
   double percent;
@@ -54,10 +80,13 @@ double cCpuStats::getCurrVal()
   statstream >> total;
 
   if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
-      totalSys < lastTotalSys || totalIdle < lastTotalIdle) {
+      totalSys < lastTotalSys || totalIdle < lastTotalIdle)
+  {
     // Overflow detection. Just skip this value.
     percent = -1.0;
-  } else {
+  }
+  else
+  {
     total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
             (totalSys - lastTotalSys);
     percent = total;
@@ -66,5 +95,22 @@ double cCpuStats::getCurrVal()
     percent *= 100;
   }
 
-  return percent;
+  std::unique_lock<std::mutex> lock(m_statMutex);
+  m_currCpuUsage = percent;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void cCpuStats::StartReadThread()
+{
+  m_statThread = std::make_shared<std::thread>(std::thread(LaunchObserver));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void cCpuStats::LaunchObserver()
+{
+  while(1)
+  {
+    UpdateCpuUsage();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
 }
